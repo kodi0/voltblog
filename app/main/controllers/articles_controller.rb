8d62424ca0c28a0ff4d22a905f52b3351c981ae6
Viktor Lazarev <taurus101v@gmail.com>
2014-12-20 21:11:37 +0400

    Changes in articles

--- app/main/controllers/articles_controller.rb
+++ app/main/controllers/articles_controller.rb
@@ -1,38 +1,42 @@
-class ArticlesController < Volt::ModelController
+require 'main/controllers/app_controller'
+
+class ArticlesController < AppController
+
   def index
-    # Add code for when the index view is loaded
   end
 
   def show
-    # Add code for when the about view is loaded
-     store._articles.find_one(_id: params._id)
+    store._articles.find_one(_id: params._id)
   end
 
   def update
   end
 
   def create
-    article = Article.new(title: page._article._title, subject: page._article._subject, user: "Roman")
+    user_id = current_user._id if logged_in?
+    article = Article.new(title: page._article._title, subject: page._article._subject, created_at: Time.now, user_id: user_id)
     store._articles << article
     page._article._title=''
     page._article._subject=''
   end
 
-  def delete article
+  def delete(article)
     store._articles.delete(article)
   end
-  
-  private
-    # The main template contains a #template binding that shows another
-    # template.  This is the path to that template.  It may change based
-    # on the params._controller and params._action values.
-    def main_path
-      params._controller.or('main') + '/' + params._action.or('index')
-    end
 
-    # Determine if the current nav component is the active one by looking
-    # at the first part of the url against the href attribute.
-    def active_tab?
-      url.path.split('/')[1] == attrs.href.split('/')[1]
-    end
+  private
+
+  # The main template contains a #template binding that shows another
+  # template.  This is the path to that template.  It may change based
+  # on the params._controller and params._action values.
+  def main_path
+    params._controller.or('main') + '/' + params._action.or('index')
+  end
+
+  # Determine if the current nav component is the active one by looking
+  # at the first part of the url against the href attribute.
+  def active_tab?
+    url.path.split('/')[1] == attrs.href.split('/')[1]
+  end
+
 end
