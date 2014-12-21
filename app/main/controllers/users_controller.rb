02ff9e467ddb31cf6cc5822e2acbb4bfbdde6980
Roman <felixcon@gmail.com>
2014-12-21 10:01:15 +0000

    final changes

--- app/main/controllers/users_controller.rb
+++ app/main/controllers/users_controller.rb
@@ -4,9 +4,27 @@ class UsersController < AppController
 
   def show
   end
-
+  
+  def create
+    user_id = current_user._id if logged_in?
+    article = Article.new(title: page._article._title, subject: page._article._subject, created_at: Time.now, user_id: user_id)
+    store._articles << article
+    page._article._title=''
+    page._article._subject=''
+  end
+  
+  def update
+  end
+  
+  def delete(article)
+    store._articles.delete(article)
+  end
+  
   def user
     find_user params._id
+  end
+  
+  def show_current_user
   end
 
 end
--- config/app.rb
+++ config/app.rb
@@ -9,10 +9,10 @@ Volt.configure do |config|
   # Database config all start with db_ and can be set either in the config
   # file or with an environment variable (DB_NAME for example).
 
-  # config.db_driver = 'mongo'
-  # config.db_name = (config.app_name + '_' + Volt.env.to_s)
-  # config.db_host = 'localhost'
-  # config.db_port = 27017
+  config.db_driver = 'mongo'
+  config.db_name = (config.app_name + '_' + Volt.env.to_s)
+  config.db_host = 'fellz-volt-1170168'
+  config.db_port = 27017
 
   # Compression options
 
