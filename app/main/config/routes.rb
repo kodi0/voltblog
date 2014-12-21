02c308d33645827d0aa7adaa31b94558c19d4141
Viktor Lazarev <taurus101v@gmail.com>
2014-12-21 12:10:44 +0400

    Show all articles at home page

--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -1,7 +1,6 @@
 # See https://github.com/voltrb/volt#routes for more info on routes
 
 get '/about', _action: 'about'
-get '/articles', _controller: 'articles' , _action: 'index'
 get '/articles/{{__id}}', _controller: 'articles', _action: 'show'
 # Routes for login and signup, provided by user-templates component gem
 get '/signup', _controller: 'user-templates', _action: 'signup'
@@ -11,4 +10,4 @@ get '/users/{{__id}}', _controller: 'users', _action: 'show'
 
 # The main route, this should be last. It will match any params not
 # previously matched.
-get '/', {}
+get '/', _controller: 'articles' , _action: 'index'
