ead8eff5bba6ae9202a5edfab2a2d485a3ae4ea5
Roman <felixcon@gmail.com>
2014-12-22 11:09:22 +0000

    added comments

--- app/main/controllers/users_controller.rb
+++ app/main/controllers/users_controller.rb
@@ -7,24 +7,35 @@ class UsersController < AppController
 
   def account
   end
-
+  # mostly helper method
+  # returns user object by id
   def user
     find_user params._id
   end
-
+  # show current_user articles at /account
   def my_articles
     store._articles.find user_id: current_user._id
   end
-
+  
+  # Edit article
+  # Read the buffer of article
+  # and assign it tmp model
+  # After editing call save! on model
   def edit_article(article)
     self.model = article.buffer
   end
-
+  # model for new article ?
+  # _new_article property for canceling creatiion of article
+  # in cancel call we assign _new_article = false
   def new_article
-    self.model = store._articles.buffer
-    page._new_article = true
+    self.model = store._articles.buffer # why buffer for articles? 
+    page._new_article = true            
   end
 
+  # If this is new article ( _new_article is true) then add properties
+  # set _new_article to false
+  # and save model
+  # then clear model with model=nil
   def save!
     if page._new_article
       self.model._created_at = Time.now
@@ -39,7 +50,9 @@ class UsersController < AppController
   def delete_article(article)
     store._articles.delete article
   end
-
+  # Cancel editing or creating new article
+  # assign model nil
+  # and new article false
   def cancel
     self.model = nil
     page._new_article = false
